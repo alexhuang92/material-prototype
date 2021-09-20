2c0dbe4bd4fc3c227ca5f6805376105275fae58a
Alex Huang <alexhuang@MacBook-Pro.local>
2021-09-20 07:03:52 -0700

    add skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'proto',
+    loadChildren: () =>
+      import('./proto/proto.module').then((m) => m.ProtoModule),
+  },
+  {
+    path: 'listing',
+    loadChildren: () =>
+      import('./listing/listing.module').then((m) => m.ListingModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'listing',
+  },
+];
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
